6adfa752894269ea2cf7814e654b93cb7b7b3445
Francisco UM <pacoum88@gmail.com>
2025-04-05 15:06:10 +0200

    hasta antes de extraer método

--- lib/pages/home.dart
+++ lib/pages/home.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_svg/flutter_svg.dart';
 
 class HomePage extends StatelessWidget {
   const HomePage({super.key});
@@ -18,6 +19,40 @@ class HomePage extends StatelessWidget {
         centerTitle: true,
         backgroundColor: Colors.white,
         elevation: 0,
+        leading: Container(
+          margin: EdgeInsets.all(10),
+          alignment: Alignment.center,
+          decoration: BoxDecoration(
+            color: Color(0xFFF7F8F8),
+            borderRadius: BorderRadius.circular(10),
+          ),
+          child: SvgPicture.asset(
+            'assets/icons/Arrow - Left 2.svg',
+            height: 20,
+            width: 20,
+          ),
+        ),
+        actions: [
+          GestureDetector(
+          onTap: () {
+            print("object");
+          },
+          child: Container(
+          margin: EdgeInsets.all(10),
+          width: 37,
+          alignment: Alignment.center,
+          decoration: BoxDecoration(
+            color: Color(0xFFF7F8F8),
+            borderRadius: BorderRadius.circular(10),
+          ),
+          child: SvgPicture.asset(
+            'assets/icons/dots.svg',
+            height: 4,
+            width: 4,
+          ),
+        ),
+        ),
+        ],
       ),
     );
   }
