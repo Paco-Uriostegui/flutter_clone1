a2dd772bed0a50b97fbb964f5ce7d19e91b796eb
Francisco UM <pacoum88@gmail.com>
2025-04-05 15:55:18 +0200

    por fin extraido método de AppBar personalizada

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_clon1/pages/home.dart';
+import 'package:flutter_svg/flutter_svg.dart';
 
 void main() {
   runApp(const MyApp());
@@ -11,10 +12,60 @@ class MyApp extends StatelessWidget {
   // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
+      return MaterialApp(
       debugShowCheckedModeBanner: false,
       theme: ThemeData(fontFamily: 'Poppins'),
       home: HomePage(),
     );
   }
 }
+
+AppBar buildAppBarPersonalizado() {  // 👈 Método público (sin "_")
+  return AppBar(
+    title: const Text(
+      'Desayuno',
+      style: TextStyle(
+        fontSize: 18,
+        fontWeight: FontWeight.bold,
+        color: Colors.black,
+      ),
+    ),
+    centerTitle: true,
+    backgroundColor: Colors.white,
+    elevation: 0,
+    leading: Container(
+      margin: const EdgeInsets.all(10),
+      alignment: Alignment.center,
+      decoration: BoxDecoration(
+        color: const Color(0xFFF7F8F8),
+        borderRadius: BorderRadius.circular(10),
+      ),
+      child: SvgPicture.asset(
+        'assets/icons/Arrow - Left 2.svg',
+        height: 20,
+        width: 20,
+      ),
+    ),
+    actions: [
+      GestureDetector(
+        onTap: () {},
+        child: Container(
+          margin: const EdgeInsets.all(10),
+          width: 37,
+          alignment: Alignment.center,
+          decoration: BoxDecoration(
+            color: const Color(0xFFF7F8F8),
+            borderRadius: BorderRadius.circular(10),
+          ),
+          child: SvgPicture.asset(
+            'assets/icons/dots.svg',
+            height: 4,
+            width: 4,
+          ),
+        ),
+      ),
+    ],
+  );
+}
+// AppBar buildAppBarPersonalizada() {
+//   return AppBar(
--- lib/pages/home.dart
+++ lib/pages/home.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
-import 'package:flutter_svg/flutter_svg.dart';
+import 'package:flutter_clon1/main.dart';
+
 
 class HomePage extends StatelessWidget {
   const HomePage({super.key});
@@ -7,51 +8,7 @@ class HomePage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: const Text(
-          'Desayuno',
-          style: TextStyle(
-            fontSize: 18,
-            fontWeight: FontWeight.bold,
-            color: Colors.black,
-          ),
-        ),
-        centerTitle: true,
-        backgroundColor: Colors.white,
-        elevation: 0,
-        leading: Container(
-          margin: EdgeInsets.all(10),
-          alignment: Alignment.center,
-          decoration: BoxDecoration(
-            color: Color(0xFFF7F8F8),
-            borderRadius: BorderRadius.circular(10),
-          ),
-          child: SvgPicture.asset(
-            'assets/icons/Arrow - Left 2.svg',
-            height: 20,
-            width: 20,
-          ),
-        ),
-        actions: [
-          GestureDetector(
-          onTap: () {},
-          child: Container(
-          margin: EdgeInsets.all(10),
-          width: 37,
-          alignment: Alignment.center,
-          decoration: BoxDecoration(
-            color: Color(0xFFF7F8F8),
-            borderRadius: BorderRadius.circular(10),
-          ),
-          child: SvgPicture.asset(
-            'assets/icons/dots.svg',
-            height: 4,
-            width: 4,
-          ),
-        ),
-        ),
-        ],
-      ),
+      appBar: buildAppBarPersonalizado(),
     );
   }
 }
