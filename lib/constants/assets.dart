89c50884ab7d610be92df786124efd371a51f211
Francisco UM <pacoum88@gmail.com>
2025-04-06 00:09:38 +0200

    agregados íconos dentro del TextField

--- lib/constants/assets.dart
+++ lib/constants/assets.dart
@@ -1,4 +1,6 @@
 class AppAssets {
   static const arrowLeft = 'assets/icons/Arrow - Left 2.svg';
   static const dots = 'assets/icons/dots.svg';
+  static const filter = 'assets/icons/Filter.svg';
+  static const search = 'assets/icons/Search.svg';
 }
--- lib/pages/home.dart
+++ lib/pages/home.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_clon1/constants/assets.dart';
 import 'package:flutter_clon1/main.dart';
+import 'package:flutter_svg/flutter_svg.dart';
 
 
 
@@ -10,6 +12,43 @@ class HomePage extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: buildAppBarPersonalizado(),
+      body: Column(children: [
+        Container(
+          margin: EdgeInsets.only(top: 40, left: 20, right: 20),
+          decoration: BoxDecoration(
+            boxShadow: [BoxShadow(
+              color: Colors.black.withOpacity(0.11),
+              blurRadius: 40,
+              spreadRadius: 0,
+            )
+        ]
+        ),
+          child: TextField(
+            decoration: InputDecoration(
+              filled: true,
+              fillColor: Colors.white,
+              prefixIcon: Padding(
+                padding: const EdgeInsets.all(12.0),
+                child: SvgPicture.asset(
+                  AppAssets.search
+                ),
+              ),
+              suffixIcon: Padding(
+                padding: const EdgeInsets.all(12.0),
+                child: SvgPicture.asset(
+                  AppAssets.filter,
+                  height: 20,
+                  width: 20,
+                ),
+              ),
+              border: OutlineInputBorder(
+                borderRadius: BorderRadius.circular(15),
+                borderSide: BorderSide.none,
+              ),
+              contentPadding: EdgeInsets.all(15),
+            ),),
+        )
+      ],),
     );
   }
 }
